3a3019b0d23741354f00b469e99dd0a04461abd8
Kirill Revunov <krrevunov@gmail.com>
2025-03-04 20:57:15 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
       <h1 className="flex text-3xl font-bold text-red-500 underline">
         Hello world!
       </h1>
+      <h1 className="flex text-3xl font-bold text-red-500 underline">
+        Hello world 2!
+      </h1>
     </>
   );
 }
