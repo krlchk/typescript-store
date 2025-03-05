5e5c5fd07cd4a39c66e59067f5a5bcc684df725a
Kirill Revunov <krrevunov@gmail.com>
2025-03-05 20:26:03 +0200

    final commit

--- src/context/shopping-cart-context.tsx
+++ src/context/shopping-cart-context.tsx
@@ -23,6 +23,7 @@ type CartItem = {
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
